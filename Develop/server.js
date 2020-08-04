f9c8fedfb5a7f5b312ad2d12c9bfe2537e08b482
Irma Georgia <irmamodzg@gmail.com>
2020-08-03 22:27:48 -0400

    deploy heroku

--- Develop/server.js
+++ Develop/server.js
@@ -24,6 +24,7 @@ app.get("/api/notes", (req, res) => {
   let note = readfile
   res.json(note)
 });
+
 // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 app.post("/api/notes", (req, res) => {
   let note = readfile
@@ -39,7 +40,8 @@ app.post("/api/notes", (req, res) => {
 });
 // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete.
 // This means you'll need to find a way to give each note a unique id
-// when it's saved.In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+// when it's saved.In order to delete a note, you'll need to read all notes from the db.json file, 
+// remove the note with the given id property, and then rewrite the notes to the db.json file.
 app.delete("/api/notes/:id", (req, res) => {
   let note = readfile
   let choosen = req.params.id
